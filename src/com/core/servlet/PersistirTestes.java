29b947c04ccf160842550688753c2d5265beb772
Jorge Maria de Freitas Neto <orck10@gmail.com>
2018-08-06 22:55:29 -0300

    Recebendo Json

--- src/com/core/servlet/PersistirTestes.java
+++ src/com/core/servlet/PersistirTestes.java
@@ -1,16 +1,17 @@
 package com.core.servlet;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import com.api.modelo.Profissional;
 import com.core.servico.ProfissionalServicoImple;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 
 public class PersistirTestes extends HttpServlet{
@@ -18,7 +19,7 @@ public class PersistirTestes extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
 	@Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		
 		String nomeUsuario = req.getHeader("nomeUsuario");
         String senha = req.getHeader("senha");
@@ -38,11 +39,29 @@ public class PersistirTestes extends HttpServlet{
 			    }
 			    String data = buffer.toString();
 			    
-			    System.out.println(data);
-			    System.out.println(data);
+			    JsonParser jsonParser = new JsonParser();
+			    JsonObject object = jsonParser.parse(data).getAsJsonObject();
+			    String crianca = object.get("cricanca").getAsString();
+			    JsonArray array = object.get("resultados").getAsJsonArray();
+			    
+			    System.out.println(crianca);
+			    for(Object n : array) {
+			    	JsonArray r = jsonParser.parse(n.toString()).getAsJsonArray();
+			    	for(Object i: r) {
+			    		System.out.println(i);	
+			    	}
+			    }
+			    
+			    
+			    resp.setContentType("application/json");
+			    resp.setCharacterEncoding("UTF-8");
+			    resp.getWriter().write("{\"error\":\"false\"}");
 			    
 			}
-			catch(Exception e){	
+			catch(Exception e){
+				resp.setContentType("application/json");
+			    resp.setCharacterEncoding("UTF-8");
+			    resp.getWriter().write("{\"error\":\"true\"}");
 			}
 		}
 	}
